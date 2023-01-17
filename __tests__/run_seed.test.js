8d1cc5595852d420e8ec36dd14062e2024f820a1
Mougily <92stephaniejones@gmail.com>
2023-01-17 10:30:53 +0000

    setting up next test

--- __tests__/run_seed.test.js
+++ __tests__/run_seed.test.js
@@ -14,7 +14,7 @@ afterAll(() => {
 
 describe("App testing", () => {
   describe("GET ", () => {
-    test.only("Returns a 200 status and an array of all topics", () => {
+    test("Returns a 200 status and an array of all topics", () => {
       return request(app)
         .get("/api/topics")
         .expect(200)
@@ -30,5 +30,28 @@ describe("App testing", () => {
           });
         });
     });
+    test("Returns a 200 status and responds with an article object based on passed article_id", () => {
+        return request(app)
+        .get("/api/articles/article_id=3")
+        .expect(200)
+        .then(({body}) => {
+            const article3 = {
+                title: 'Eight pug gifs that remind me of mitch',
+                topic: 'mitch',
+                author: 'icellusedkars',
+                body: 'some gifs',
+                created_at: 1604394720000,
+                article_img_url:
+                  'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
+                  article_id : 3,
+                  votes : 0
+              }
+            expect(body.articles.length).toBeGreaterThan(0);
+            body.article.forEach((article) => {
+                expect(article).toEqual(article3)                
+            })
+        })
+    })
+
   });
 });
--- model.js
+++ model.js
@@ -11,4 +11,6 @@ const fetchAllTopics = () => {
     })
 }
 
+
+
 module.exports = {fetchAllTopics}
